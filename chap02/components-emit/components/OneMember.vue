0c4bd3cba5a2516212600e2e0dd7df09fcb5a7fb
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-11-11 16:16:44 +0900

    components-emitの学習中をコミット

---
+++ chap02/components-emit/components/OneMember.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+interface Props {
+    id: number;
+    name: string;
+    email: string;
+    points: number;
+    note?: string;
+}
+
+// Emits型を定義
+type Emits = {
+    incrementPoint: [id: number];
+};
+</script>
+<template>
+
+</template>
+<style scoped>
+.box {
+    border: green 1px solid;
+    margin: 10px;
+}
+</style>
