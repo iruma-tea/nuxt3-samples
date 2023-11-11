100ad9e5d2adfef925db501998af2b81957473d7
iruma-tea <shusuke.think.pad@gmail.com>
2023-11-11 18:59:39 +0900

    components-emitの学習完了

--- chap02/components-emit/app.vue
+++ chap02/components-emit/app.vue
@@ -1,5 +1,51 @@
+<script setup lang="ts">
+// 会員リストデータを用意
+const memberListInit = new Map<number, Member>();
+memberListInit.set(33456, {id: 22456, name: "田中太郎", email: "bow@example.com", points: 35, note: "初回特典あり。"});
+memberListInit.set(47783, {id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53});
+const memberList = ref(memberListInit);
+
+// 会員リスト内の会員のポイント合計
+const totalPoints = computed(
+  (): number => {
+    let total = 0;
+    for (const member of memberList.value.values()) {
+      total += member.points;
+    }
+    return total;
+  }
+);
+
+//Emitにより実行されるメソッド。
+const onIncrementPoint = (id: number): void => {
+  const member = memberList.value.get(id);
+  if (member != undefined) {
+    member.points++;
+  }
+}
+
+interface Member {
+  id: number;
+  name: string;
+  email: string;
+  points: number;
+  note?: string;
+}
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <section>
+    <h1>会員リスト</h1>
+    <p>全会員の保有ポイントの合計: {{ totalPoints }}</p>
+    <OneMember 
+      v-for="[id, member] in memberList"
+      v-bind:key="id"
+      v-bind:id="id"
+      v-bind:name="member.name"
+      v-bind:email="member.email"
+      v-bind:points="member.points"
+      v-bind:note="member.note"
+      v-on:increment-point="onIncrementPoint"
+    />
+  </section>
 </template>
--- chap02/components-emit/components/OneMember.vue
+++ chap02/components-emit/components/OneMember.vue
@@ -11,9 +11,43 @@ interface Props {
 type Emits = {
     incrementPoint: [id: number];
 };
+
+// Propオブジェクトの設定
+const props = defineProps<Props>();
+//Emitの設定
+const emit = defineEmits<Emits>();
+
+// Propsのnoteを加工する算出プロパティ
+const localNote = computed(
+    ():string => {
+        let localNote = props.note;
+        if (localNote == undefined) {
+            localNote = "--";
+        }
+        return localNote;
+    }
+);
+
+// [ポイント加算]ボタンをクリックした時の処理
+const pointUp = ():void => {
+    emit("incrementPoint", props.id);
+}
 </script>
 <template>
-
+    <section class="box">
+        <h4>{{ name }}さんの情報</h4>
+        <dl>
+            <dt>ID</dt>
+            <dd>{{ id }}</dd>
+            <dt>メールアドレス</dt>
+            <dd>{{ email }}</dd>
+            <dt>保有ポイント</dt>
+            <dd>{{ points }}</dd>
+            <dt>備考</dt>
+            <dd>{{ localNote }}</dd>
+        </dl>
+        <button v-on:click="pointUp">ポイントアップ</button>
+    </section>
 </template>
 <style scoped>
 .box {
