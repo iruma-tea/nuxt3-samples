f5cdc6b257efcaa56889cbe81f44c65c0395fd18
iruma-tea <shusuke.think.pad@gmail.com>
2023-12-09 20:08:47 +0900

    routing-paramsのサンプルを追加

---
+++ chap03/routing-params/pages/member/search/[name]/[points].vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>ルートパラメータ</h1>
+    <nav id="breadcrumbs">
+        <ul>
+            <li>
+                <NuxtLink v-bind:to="{name : 'index'}">TOP</NuxtLink>
+            </li>
+            <li>ルーとパラメータ参照</li>
+        </ul>
+    </nav>
+    <section>
+        <p>{{ $route.params }}</p>
+    </section>
+</template>
